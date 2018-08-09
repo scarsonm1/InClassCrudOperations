a70eb35326e68a7695f9c10ec618724ab3c2b479
Shir <healthylegacy01@gmail.com>
2018-08-09 13:39:09 -0500

    Add project files.

---
+++ InClassCrud Operations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InClassCrud_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
