2a3db70d5ad46bc08541b6e1919bf8bc3703cbe4
Shir <healthylegacy01@gmail.com>
2018-09-05 17:19:26 -0500

    LOC refactored

--- InClassCrud Operations/Program.cs
+++ InClassCrud Operations/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
 
 namespace InClassCrud_Operations
 {
@@ -6,7 +10,41 @@ namespace InClassCrud_Operations
     {
         static void Main(string[] args)
         {
-           ;
+            IConfiguration configBuilder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+#if DEBUG
+                    .AddJsonFile("appsettings.debug.json")
+#else
+                    .AddJsonFile("appsettings.release.json")
+#endif
+                    .Build();
+
+            string ConnectionString = configBuilder.GetConnectionString("DefaultConnection");
+            //Create
+            Console.WriteLine("What category do you want to add?");
+
+            string Category = Console.ReadLine();
+
+            Console.WriteLine("What is the most you want to pay for a product today?");
+            int maxPrice = int.Parse(Console.ReadLine());
+            Repository repository = new Repository(ConnectionString);
+            List<string> values = repository.GetProductsNameInPriceRange(maxPrice);
+
+            foreach (string value in values)
+            {
+                Console.WriteLine(value);
+            }
+
+            Console.ReadLine();
+
+            Console.WriteLine("What do you want the price of the Samsung Galaxy to be?");
+            int newPrice = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("What product do you want to delete?");
+            string productToDelete = Console.ReadLine();
+
         }
+       
+
     }
 }
---
+++ InClassCrud Operations/Repository.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace InClassCrud_Operations
+{
+    class Repository
+    {
+        public Repository (string connection )
+        {
+            connectionstring = connection;  
+        }
+        string connectionstring;
+        // Read
+        public void AddNewCategory(string Category)
+        {
+            string connString = connectionstring;
+
+            MySqlConnection conn = new MySqlConnection(connString);
+
+            using (conn)
+            {
+                conn.Open();
+
+                MySqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "INSERT INTO categories (Name) VALUES (@newCategory)";
+                cmd.Parameters.AddWithValue("newCategory", Category);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        // Update
+        public List<string> GetProductsNameInPriceRange(int maxPrice)
+        {
+            string connString = connectionstring;
+
+            MySqlConnection conn = new MySqlConnection(connString);
+
+            using (conn) 
+            {
+
+                conn.Open();
+
+                MySqlCommand cmd = conn.CreateCommand();
+
+                MySqlDataReader dr = cmd.ExecuteReader();
+
+                List<string> productNames = new List<string>();
+
+                while (dr.Read())
+                {
+                    productNames.Add(dr["Name"].ToString());
+                }
+                return productNames;
+
+            }
+        }
+        public void PriceChange(int Price)
+        {
+            string connString = connectionstring;
+
+            MySqlConnection conn = new MySqlConnection(connString);
+            using (conn)
+            {
+                conn.Open();
+
+                MySqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "UPDATE Samsung Galaxy From products SET Price =@newPrice";
+                cmd.Parameters.AddWithValue("newPrice", Price);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+
+        // Delete
+        public void DeleteProduct(string productToDelete)
+        {
+
+            string connString = connectionstring;
+
+            MySqlConnection conn = new MySqlConnection(connString);
+
+
+            using (conn)
+            {
+                conn.Open();
+
+                MySqlCommand cmd = conn.CreateCommand();
+
+                cmd.CommandText = "DELETE FROM products WHERE Name LIKE '%productToDelete";
+
+                cmd.Parameters.AddWithValue("productToDelete", productToDelete);
+
+                cmd.ExecuteNonQuery();
+            }
+
+        }
+    }
+}
+
+
+
+            
